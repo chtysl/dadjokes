011f29102f32604fdca435216d568629e4099fc8
chtysl <devCihat@gmail.com>
2021-01-17 23:50:24 +0300

    init

---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h2>Corny dad jokes</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: "Welcome to dad jokes.",
+      meta: [
+        {
+          hid: "decription",
+          name: "decription",
+          content: "Best place for dad jokes",
+        },
+      ],
+    };
+  },
+};
+</script>
